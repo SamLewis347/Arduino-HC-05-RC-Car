a49b91fe608fe4c1b122bd2797e4d10ac904edf6
SamLewis347 <120208331+SamLewis347@users.noreply.github.com>
2023-12-08 13:14:09 -0500

    Update Arduino_Master.ino

--- Arduino_Master.ino
+++ Arduino_Master.ino
@@ -2,7 +2,7 @@ int joystickX = A0;  // X-axis of joystick connected to A0
 int joystickY = A1;  // Y-axis of joystick connected to A1
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);  // Default baud rate for HC-05
+  Serial.begin(9600);  // Beginning serial connections for use between the two HC-05 modules
 }
 bool flash = false;
 void loop() {
@@ -11,8 +11,8 @@ void loop() {
   if (yValue < 550) {
     yValue = 550;
   }
-  int angle = map(xValue, 0, 1023, 0, 180);  // Map Y-axis to ESC speed (0-180 range)  // Map X-axis to servo angle
-  int speed = map(yValue, 550, 1023, 90, 180);  // Map Y-axis to ESC speed (0-180 range)
+  int angle = map(xValue, 0, 1023, 0, 180); // Map X-axis to servo angle
+  int speed = map(yValue, 550, 1023, 90, 180);  // Map Y-axis to ESC speed (90-180 range) // Needs to be 90 to arm/calibrate the esc
   Serial.write(255);
   delay(25);
   Serial.write(angle);
